009294094e62f734f2831988004159d36a229217
juniperlsimonis <dapperstats@gmail.com>
2021-05-28 13:25:09 -0700

    tiny

--- book_build.R
+++ book_build.R
@@ -10,3 +10,5 @@
 bookdown::render_book(input = "index.Rmd", output_format = "bookdown::pdf_book")
 
 bookdown::render_book(input = "index.Rmd", output_format = "bookdown::gitbook")
+
+bookdown::publish_book(account = "simonis")

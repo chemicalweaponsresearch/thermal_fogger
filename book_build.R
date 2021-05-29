27a7a4fa362ea4de6e70b83fe10c8ee38902ed55
juniperlsimonis <dapperstats@gmail.com>
2021-05-28 18:14:58 -0700

    Update book_build.R

--- book_build.R
+++ book_build.R
@@ -1,8 +1,12 @@
 #
-# Book Build Script
+# Book Build Script for The Thermal Fogger: An Imperial Tetherball
 #
-# v0.3.0 
 # J L Simonis
+#
+# v0.3.0 2021-05-18
+#
+
+
 #
 # The pdf should be built first so that it is available to the gitbook
 #
